95b6aecd516e20c569d5f1320ab3ea26b8c368f2
Antonio Román <kyradiscord@gmail.com>
2021-07-24 20:03:09 +0200

    chore: git :/

--- src/lib/ByteBuffer.ts
+++ src/lib/ByteBuffer.ts
@@ -1,13 +1,13 @@
 export class ByteBuffer {
 	private written = 0;
-	private data: Uint8Array;
+	private data: Buffer;
 
 	/**
 	 * Constructs the instance.
 	 * @param size The amount of bytes to reserve, defaults to 8KB.
 	 */
 	public constructor(size = 8192) {
-		this.data = new Uint8Array(size);
+		this.data = Buffer.allocUnsafe(size);
 	}
 
 	/**
@@ -29,7 +29,7 @@ export class ByteBuffer {
 	 * Writes a single byte into the buffer.
 	 * @param byte The byte to write, between `0x00` and `0xFF`.
 	 */
-	public writeByte(byte: number) {
+	public writeByte(byte: number): void {
 		this.ensureByte();
 		this.data[this.written++] = byte;
 	}
@@ -63,7 +63,7 @@ export class ByteBuffer {
 	 * Gets a sub-array of what was written so far.
 	 * @returns The written section of the data.
 	 */
-	public toArray(): Uint8Array {
+	public toArray(): Buffer {
 		return this.data.subarray(0, this.written);
 	}
 
@@ -73,26 +73,26 @@ export class ByteBuffer {
 	 * @param start The start index, defaults to `0`.
 	 * @param end The end index, defaults to {@link Uint8Array.length `this.data.length`}.
 	 */
-	public fill(byte: number, start?: number, end?: number) {
+	public fill(byte: number, start?: number, end?: number): void {
 		this.data.fill(byte, start, end);
 	}
 
-	private ensureByte() {
+	private ensureByte(): void {
 		if (this.written + 1 >= this.data.length) {
 			const size = this.data.length * 2;
 			this.data = this.copyBytes(size);
 		}
 	}
 
-	private ensureBytes(n: number) {
+	private ensureBytes(n: number): void {
 		if (this.written + n >= this.data.length) {
 			const size = Math.pow(2, Math.ceil(Math.log(this.written + n) / Math.log(2)));
 			this.data = this.copyBytes(size);
 		}
 	}
 
-	private copyBytes(size: number) {
-		const data = new Uint8Array(size);
+	private copyBytes(size: number): Buffer {
+		const data = Buffer.allocUnsafe(size);
 
 		for (let i = 0; i < this.written; ++i) {
 			data[i] = this.data[i];
